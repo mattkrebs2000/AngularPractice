ac4704f56c597f06a474034d3f0563e3682c9430
mattkrebs1974 <mattkrebsemail@gmail.com>
2020-08-06 12:35:39 -0400

    second component added

---
+++ AngularTraversy/angular-crash-todolist/src/app/components/todos/todos.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { Todo } from '../../models/Todo';
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css']
+})
+export class TodosComponent implements OnInit {
+
+  constructor() { }
+
+  todos: Todo[];
+
+  // tslint:disable-next-line: typedef
+  ngOnInit() {
+
+    this.todos = [{
+
+      id: 1,
+      title: 'Todo One',
+      completed: false,
+    },
+      {
+
+        id: 2,
+        title: 'Todo Two',
+        completed: true,
+      },
+      {
+
+        id: 3,
+        title: 'Todo Three',
+        completed: false,
+      }
+
+  ];
+
+
+
+  }
+
+}
